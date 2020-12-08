4dee93aed6968645af67c5267d25785c3535ab4d
fumi uk <dcfumiya.238@gmail.com>
2020-12-09 07:59:24 +0900

    async

---
+++ src/actions/counterActions.js
@@ -0,0 +1,11 @@
+export function increment() {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch({ type: "INCREMENT" });
+    }, 2000);
+  }
+}
+
+export function decrement() {
+  return { type: "DECREMENT" };
+}
